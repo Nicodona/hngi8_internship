fc4c00bca4189d5f6aa488246c50d4373edbd437
njei nicodona <njeinicodona@gmail.com>
2021-08-17 19:47:04 +0100

    complete logic

--- resume/demoresume/models.py
+++ resume/demoresume/models.py
@@ -5,13 +5,13 @@ class Data(models.Model):
     email = models.EmailField(max_length=256)
     phone = models.CharField(max_length=12)
     date_of_birth = models.CharField(max_length=20)
-    description = models.TextField
+    description = models.TextField( default="hellow world")
     job = models.CharField(max_length=512)
     degree = models.CharField(max_length=256)
     dod = models.CharField(max_length=20)
     experience = models.CharField(max_length=512)
     YoE = models.CharField(max_length=20)
-    job_desc = models.TextField
+    job_desc = models.TextField(default="yoo bro")
 
 
     def __str__(self):
--- resume/demoresume/views.py
+++ resume/demoresume/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Data
 
 def home(request):
     if request.method == 'POST':
-        pass
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        date_of_birth = request.POST['dob']
+        description = request.POST['description']
+        job = request.POST['job']
+        degree = request.POST['degree']
+        dod = request.POST['dod']
+        experience = request.POST['job_experience']
+        YoE = request.POST['YoE']
+        jd = request.POST['jd']
+
+        new = Data(name=name, email=email, phone=phone, date_of_birth=date_of_birth, description=description, job=job, degree=degree, dod=dod, experience=experience, YoE=YoE, job_desc=jd)
+        new.save()
+        return redirect('/')
+
     else:
-        return render(request, 'home.html')
+        record = Data.objects.all()
+        return render(request, 'home.html', {'records': record})
