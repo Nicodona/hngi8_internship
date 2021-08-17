df1a15b2d1276af03a9c1179e11442857a3cbfaf
njei nicodona <njeinicodona@gmail.com>
2021-08-17 17:12:56 +0100

    new app

--- resume/demoresume/models.py
+++ resume/demoresume/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Data(models.Model):
+    name = models.CharField(max_length=252)
+    email = models.EmailField(max_length=256)
+    phone = models.CharField(max_length=12)
+    date_of_birth = models.CharField(max_length=20)
+    description = models.TextField
+    job = models.CharField(max_length=512)
+    degree = models.CharField(max_length=256)
+    dod = models.CharField(max_length=20)
+    experience = models.CharField(max_length=512)
+    YoE = models.CharField(max_length=20)
+    job_desc = models.TextField
+
+
+    def __str__(self):
+        return self.name
--- resume/demoresume/views.py
+++ resume/demoresume/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'home.html')
